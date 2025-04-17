cc60a44cb69cd9ab4bfd77ef37f22cc7bddc62b3
MONARCH <ma0sadao519@gmail.com>
2025-04-17 21:47:36 +0900

    bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram import Update, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKe
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
 # 🔐 Токен Telegram-бота
-TOKEN = "7645634931:AAG1mVufwnQ3RWDr_5EvOaXgIC9ip4XYHo4"
+TOKEN = "7645634931:AAFKi5X6AzjtZci9fZQXV0q1LQEmUTVkqYI"
 
 # 🔹 Команда /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
